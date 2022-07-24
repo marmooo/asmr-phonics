6d164507e639ebab564156ee9ff1a6a9bbfa85df
marmooo <marmooo@users.noreply.github.com>
2022-07-24 09:06:19 +0900

    bump bootstrap from 5.1.3 to 5.2.0

--- src/sw.js
+++ src/sw.js
@@ -1,10 +1,10 @@
-var CACHE_NAME = "2022-07-12 10:25";
+var CACHE_NAME = "2022-07-24 00:46";
 var urlsToCache = [
   "/asmr-phonics/",
   "/asmr-phonics/index.js",
   "/asmr-phonics/favicon/favicon.svg",
-  "https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css",
-  "https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js",
+  "https://cdn.jsdelivr.net/npm/bootstrap@5.2.0/dist/css/bootstrap.min.css",
+  "https://cdn.jsdelivr.net/npm/bootstrap@5.2.0/dist/js/bootstrap.bundle.min.js",
 ];
 
 self.addEventListener("install", function (event) {
