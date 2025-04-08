63b5b391ba3820ba8f36976a70b9c3e98ad1096f
marmooo <marmooo@users.noreply.github.com>
2025-04-08 09:25:52 +0900

    bump bootstrap from 5.3.3 to 5.3.5

--- src/index.js
+++ src/index.js
@@ -1,4 +1,4 @@
-import { Collapse } from "https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/+esm";
+import { Collapse } from "https://cdn.jsdelivr.net/npm/bootstrap@5.3.5/+esm";
 
 let problems = [];
 let problem = ["ASMR", "Phonics"];
